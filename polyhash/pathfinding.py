9ee7b25073a6574c578c6892f0c854364203c362
Anthime HUCHET <anthime.huchet.etud@gmail.com>
2020-10-16 15:07:15 +0200

    Move pathfinding.py premier test

---
+++ polyhash/pathfinding.py
@@ -0,0 +1,12 @@
+grille = []
+lines = 0
+columns = 0
+#Récupérer les coordonnées depuis le fichier de gaetan
+
+height,width = 4,5
+
+#génération de la grille remplie de zéros
+grille = [[0 for j in range (columns)] for i in range (lignes)]
+
+print(grille)
+
