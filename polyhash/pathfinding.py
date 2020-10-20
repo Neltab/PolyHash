96cd22d9a1e8189ca9476ac72ccac8fb6f3491d2
Kyyrozu <kyyrozupro@gmail.com>
2020-10-20 14:33:09 +0200

    Fonction de position des points de montage sur la grille

--- polyhash/pathfinding.py
+++ polyhash/pathfinding.py
@@ -7,12 +7,20 @@ lines,columns = 4,5
 
 #génération de la grille remplie de zéros
 grid = [[0 for j in range (columns)] for i in range (lines)]
-print(grille)
+print(grid)
+points = [2,0,2,3,4]
 
-points = [2,1,2,3,5]
+def apply_points_to_grid(g, p):
+    numberOfPoints = p[0]
+    newGrid = g
+    n = 1
+    i = 1
+    while n <= numberOfPoints : #Pour chaque point, et a chaque boucle on incrémente de 2
+        newGrid[p[i]][p[i+1]] = 1
+        n+=1
+        i+=2
+    return newGrid
 
-def apply_points_to_grid(grid, points):
-    numberOfPoints = points[0]
-    newGrid = grid
-    for i in range(numberOfPoints): #Pour chaque point
-        #modify new gfrid depending on points[]
+
+grid2 = apply_points_to_grid(grid, points)
+print(grid2)
