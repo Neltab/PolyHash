c849faa25a574b8c7c396b8fa16a42693b6c10ed
Anthime <anthime.huchet.etud@gmail.com>
2020-10-25 19:37:27 +0100

    settings.py

--- polyhash/Grid.py
+++ polyhash/Grid.py
@@ -1,3 +1,6 @@
+from polyhash import Node
+from polyhash import settings as S
+
 def apply_points_to_grid(g, p):  # retourne une grille ayant des 1 aux emplacements des points de montage et 0 sinon
     numberOfPoints = len(p) / 2
     newGrid = g
@@ -8,3 +11,19 @@ def apply_points_to_grid(g, p):  # retourne une grille ayant des 1 aux emplaceme
         n += 1
         i += 2
     return newGrid
+
+#returns all the neighbour nodes from a given node
+def GetNeighbours(node: Node) :
+    neighbours: Node = []
+    for x in range (-1, 1):
+        for y in range (-1, 1):
+            if x == 0 and y == 0:
+                continue
+            checkX = node.GridX + x
+            checkY = node.gridY + y
+
+            if checkX >= 0 and checkX < S.lines and checkY >= 0 and checkY < S.columns :
+                neighbours.append(S.grid)
+    return neighbours
+
+
