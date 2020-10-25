3b275166eba20809e10a08447fc2361b1f53983b
Aurelien Boissiere <aurelien.boissiere@etu.univ-nantes.fr>
2020-10-25 23:03:49 +0100

    Mise en forme du groupement par chaine de rendement

--- INPUT gaetan.py
+++ INPUT gaetan.py
@@ -73,6 +73,7 @@ class Taches:
 
 #Création de la boucle pour prendre les coordonnées des points d'assemblages des tables
 lcoordtask=[]
+result = []
 
 for p in range(NBTACHES - 1):
     lcoordtask = []
@@ -115,6 +116,7 @@ for p in range(NBTACHES - 1):
     rendement = pointstask/nbcase
     exec('Tache' + str(p) + '=' + 'Taches(nbcase, nbptassemblagetask, rendement, moment)')
     lcoordtask.append(moment)
+    result.append(Taches(nbcase, nbptassemblagetask, rendement, moment))
 
 #dernière ligne du fichier texte n'a pas de '/n' on la traite donc apart
 lcoordtask = []
@@ -140,6 +142,9 @@ else:
 nbcase = Calculdistance(ltempo)
 rendement = pointstask/nbcase
 exec('Tache'+str(NBTACHES-1) + '=' + 'Taches(nbcase, nbptassemblagetask, rendement, ltempo)')
+result.append(Taches(nbcase, nbptassemblagetask, rendement, ltempo))
+print(result)
+print(LPTDEMONT)
 
 
 fichier.close()
