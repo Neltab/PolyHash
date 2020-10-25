8e4ab45878ea4a67b8fef3197a7ea94b9e0963f7
Anthime <anthime.huchet.etud@gmail.com>
2020-10-25 19:55:07 +0100

    settings.py implementation in main.py

--- main.py
+++ main.py
@@ -3,18 +3,17 @@
 
 from polyhash.Node import Node
 from polyhash.Grid import apply_points_to_grid
+from polyhash import settings as S
 
 if __name__ == "__main__":
 
     #points de montage à vérifier
     target = [0,0]
-    #Lignes et colonnes de la grille
-    lines,columns = 4,5
 
     #génération de la grille remplie de zéros
-    grid = [[0 for j in range (columns)] for i in range (lines)]
-    print(grid)
+    grid = [[0 for j in range (S.columns)] for i in range (S.lines)]
+    print(S.grid)
     points = [0,2,3,4]
 
-    pointsGrid = apply_points_to_grid(grid, points)  # c'est la grille qui contient les positions de collisions
+    pointsGrid = apply_points_to_grid(S.grid, points)  # c'est la grille qui contient les positions de collisions
     print(pointsGrid)
