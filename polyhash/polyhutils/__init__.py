ff0fc0c3931f7c2e52333bbe1a9305e3cb9c0fac
Aurélien <aurelien.boissiere@etu.univ-nantes.fr>
2020-11-02 18:51:59 +0100

    Importation de la classe groupBy dans toute l'arborescence

--- polyhash/polyhutils/__init__.py
+++ polyhash/polyhutils/__init__.py
@@ -10,3 +10,4 @@
 # Par exemple, ça permet l'usage de: from .polyhutils import say_hello
 # plutôt que: from .polyhutils.polyhio import say_hello
 from .polyhio import say_hello
+from .groupBy import groupBy
