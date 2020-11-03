f197542a80d721c129a1b3db70a33d18eda389f0
Gaetan B <gaetan.balem@etu.univ-nantes.fr>
2020-11-03 21:21:04 +0100

    Remise en forme des coordonnées des taches en Liste d'objet 'Tache'

--- Input2.0.py
+++ Input2.0.py
@@ -34,12 +34,12 @@ with open("a_example.txt","r") as fichier:
     NBTACHES = caracglob[4]
     NBETAPES = caracglob[5]
 
-    #boucle pour remplir LPOINTDEMONT.
+    #Boucle pour remplir LPOINTDEMONT.
     LPOINTDEMONT = []
     for j in range(NBPTDEMONT):
         LPOINTDEMONT.append(Extracint(fichier.readline()))
 
-    #Création de la classe "Tache" pour lister les taches et leur caractéristique
+    #Création de la classe "Tache" pour lister les taches et leur caractéristique.
     class Tache:
         def __init__(self, nbcase : int, nbassemb : int, nbpoint : int, coordtask : list):
             self.nbcase = nbcase
@@ -47,6 +47,10 @@ with open("a_example.txt","r") as fichier:
             self.nbpoint = nbpoint
             self.coordtask = coordtask
 
+    #On stock chaqu'une des taches dans une liste dont l'indice de la liste = indice tache.
+    LTASK = [0 for nt in range(NBTACHES)]
+
+    #Boucle pour lire toute les taches et remplir LTASK.
     for w in range(NBTACHES):
         infotask = Extracint(fichier.readline())
         nbpoint = infotask[0]
@@ -65,4 +69,13 @@ with open("a_example.txt","r") as fichier:
             coordtask = [temp]
         nbcase = Calculdistance(coordtask)
         exec("Tache" + str(w) + " = " + "Tache(nbcase, nbassemb, nbpoint, coordtask)")
+        LTASK[w] = Tache(nbcase, nbassemb, nbpoint, coordtask)
 
+
+#### Example de fonctionnement d'appel de la liste sur le fichier a_ecample.txt:
+#### >>> print(LTASK[0].nbcase)
+#### 1
+#### Ce qui est bien la valeur du nombre de cases séparant les deux points d'assemblages de la tache 0 dans 'a_example.txt
+#### Alors, On a donc mit chaque élément de la liste en tant qu'objet de type 'Tache'
+#### >>> print(type(LTASK[0]))
+#### <class '__main__.Tache'>
